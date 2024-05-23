81b5525a91f6269b3261c2bf3b3361040bf4a43c
kevin Julian <kevin.Julian@campuscecep.edu.co>
2024-05-22 20:04:26 -0500

    Migraciones-SQL

---
+++ database/migrations/2024_05_21_201131_create_menus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id('id_menu')->unique();
+            $table->string('nombre_menu', 255);
+            $table->text('descripcion');
+            $table->integer('precio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+};
---
+++ database/migrations/2024_05_21_202718_create_empleados_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id('id_empleado')->unique();
+            $table->string('nombre_empleado', 255);
+            $table->string('apellido', 255);
+            $table->string('cargo_empleado');
+            $table->integer('salario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+};
